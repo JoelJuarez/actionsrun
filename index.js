51a8bd008e3abc10ece3270ebcf1536f10785df8
Joel Juarez <40305419+JoelJuarez@users.noreply.github.com>
2023-04-18 12:01:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const versionNameRegexPattern = /(versionName(?:\s|=)*)(.*)/;
 
 const environmentRegexPattern = /(variantFlavor(?:\s|=)*)(.*)/;
 
+const environmentRegexPattern = /(aplication(?:\s|=)*)(.*)/;
+
 
 function versionPocket () {
         try {
-        const platform = core.getInput('platform');
+       
         if (platform === 'android') {
             // path del gradle
             const gradlePath = core.getInput('gradlePath');
@@ -131,6 +133,136 @@ function versionPocket () {
 
 function versionPosMovil() {
      console.log(`versionPosMovil ---> versionPosMovil <---`); 
+        try {
+   
+    if (platform === 'android') {
+        
+        // path del gradle
+        const gradlePath = core.getInput('gradlePath');
+        //version actual
+        const versionName = core.getInput('versionNumber');
+        //ambiente seleccionado
+        const typeEnvironment = core.getInput('typeEnvironment');
+        // version seleccionada
+        const versionInput = core.getInput('numberVersion');
+         //commit message
+        const commitMessage = core.getInput('commitMessage');
+         
+         console.log(`commitMessage -> ${commitMessage}`);
+         var versionComment = 0
+        if (environmentRegexPattern.test(commitMessage)){
+           var versionComment =  getCommentValue(commitMessage);
+        }
+        
+       setEnvironment(typeEnvironment)
+        
+        
+        console.log(`gradlePath -> ${gradlePath}`);
+        console.log(`versionComment -> ${versionComment}`);
+        
+        var versionParts = versionName.split('.');
+         console.log(`succes : ... ${versionInput}`);
+        if (versionInput) {
+             
+           var versionParts = versionInput.split('.');
+        } else if (versionComment != 0) {
+            var versionParts = versionComment.split('.');
+        }
+        
+        let finalNewVersion = '';
+        let newVersionParts = versionParts[versionParts.length -1];
+
+        let lastPartMayor = 1;
+        let lastPartMinor = 0;
+        let lastPartVersion = 0;
+        
+        if(newVersionParts.length > 0) {
+            lastPartMayor = parseInt(versionParts[0]);
+            lastPartMinor = parseInt(versionParts[1]);
+            if(versionInput){
+                 lastPartVersion = parseInt(versionParts[2]);
+            }  else if (versionComment && versionComment != "v0")  {
+                  lastPartVersion = parseInt(versionParts[2]);
+            } else {
+                lastPartVersion = parseInt(versionParts[2]) + 1;
+            }
+           
+            
+            console.log(`lastPartMayor -> ${lastPartMayor}`);
+            console.log(`lastPartMinor -> ${lastPartMinor}`);
+            console.log(`lastPartVersion -> ${lastPartVersion}`);
+         
+         let initSing = versionName.indexOf("(");
+         let finalSing = versionName.indexOf(")");
+         let qaVersion = versionName.substring(initSing + 1, finalSing)
+            
+         core.setOutput( "qa-version-number",`${parseInt(qaVersion) + 1}`);
+            
+          console.log(`lastPartVersion -> ${versionName.substring(initSing + 1, finalSing)}`);
+            
+          
+            if(lastPartVersion > 99) {
+                lastPartVersion = 0;
+                lastPartMinor = lastPartMinor + 1;
+                if(lastPartMinor > 99) {
+                    lastPartMinor = 0;
+                    lastPartMayor = lastPartMayor + 1;
+                }
+            }
+            finalNewVersion = `${lastPartMayor}.${lastPartMinor}.${lastPartVersion}`;
+        }
+
+        let versionCode = '';
+        let versionFinalParts = finalNewVersion.split('.');
+        
+        versionFinalParts.forEach(element => {
+            let newPart = element;
+            if(element.length === 1) {
+                newPart = `${element}`;
+            }
+            versionCode = `${versionCode}${newPart}`;
+        });
+
+        fs.readFile(gradlePath, 'utf8', function (err, data) {
+         
+            if(!data) {
+                 console.log(`data is Empty ${data}`);
+                console.log(`Error : ... ${err}`);
+            return
+            }
+            
+            newGradle = data;
+            if (versionCode.length > 0){
+              newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`);
+                core.setOutput( "new-version-code",`${versionCode}`);
+            }
+               
+            if (versionName.length > 0){
+                newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${finalNewVersion}\"`);
+                console.log(`finalNewVersion: ${finalNewVersion}`);
+                core.setOutput( "new-version-number",`${finalNewVersion}`);
+            }
+                
+            fs.writeFile(gradlePath, newGradle, function (err) {
+                if (err) throw err;
+                if (versionCode.length > 0) {
+                     console.log(`Successfully override versionCode ${versionCode}`)
+                     console.log(`Version Name : ${versionCode}`);
+                }
+                   
+             
+                if (versionName.length > 0){
+                    console.log(`Successfully override versionName ${versionName}`)
+                    console.log(`Version Name : ${versionName}`);
+                }
+                
+                core.setOutput("result", `Done`);
+            });
+        });
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
 }
 
 
@@ -195,7 +327,53 @@ function validateQA (commitValue) {
   return "";
 };
 
- const appVersion = core.getInput('aplication');
+//Pos Movil Functions 
+function getCommentValue (commitValue) {
+     let variantVar = commitValue.indexOf("aplication:");
+     let closeString = commitValue.indexOf("&&");
+     let flavorValue = commitValue.substring(variantVar + 11, closeString);
+     
+    console.log(`flavorValue function -->  ${flavorValue} <---`);
+     
+    console.log(`commitMessage function -->  ${commitValue} <---`);
+    let data  = flavorValue.split('@');
+     if (data.length > 0) {
+            let environmenttype = data[0]
+            let newVersion = data[1]
+            setEnvironment(environmenttype)
+               console.log(`environmenttype.. -> ${environmenttype}`);
+            console.log(`newVersion.. -> ${newVersion}`);
+            
+          return newVersion
+      }
+     console.log(`newVersion -> 0`);
+    return 0
+
+}
+
+function setEnvironment(typeEnvironment) {
+  if(typeEnvironment == "Quality") {
+             core.setOutput( "assemble_value",`assembleDebug`);
+             core.setOutput( "final_path_apk",`uat/debug/app-uat-debug.apk`);
+            console.log(`assemble_value -> assembleDebug`);
+            console.log(`final_path_apk -> uat/debug/app-uat-debug.apk`);
+            
+        } else if(typeEnvironment == "Release") { 
+             core.setOutput( "assemble_value",`assembleProdRelease`);
+             core.setOutput( "final_path_apk",`prod/release/app-prod-release.apk`);
+            console.log(`assemble_value -> assembleProdRelease`);
+            console.log(`final_path_apk -> prod/release/app-prod-release.apk`);
+        } else {
+            core.setOutput( "assemble_value",`assembleDebug`);
+            core.setOutput( "final_path_apk",`uat/debug/app-uat-debug.apk`);
+            console.log(`assemble_value -> assembleDebug`);
+            console.log(`final_path_apk -> uat/debug/app-uat-debug.apk`);
+        }
+}
+
+
+const appVersion = core.getInput('aplication');
+const platform = core.getInput('platform');
 
 if (appVersion == "Pocket") {
     console.log(`Flow Pocket ******** `);
