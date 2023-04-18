74e091006ddb5e9962c28679984cd75107dbb640
Joel Juarez <40305419+JoelJuarez@users.noreply.github.com>
2023-04-18 09:30:25 -0600

    Update posApp.js

--- posApp.js
+++ posApp.js
@@ -1,18 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-
-
-
-
-
-try {
 
 function getPosApp() {
 
     console.log(` -- -- -- --> getPosApp <-- -- --`); 
-}
-
-} catch (error) {
-    core.setFailed(error.message);
 }
